86026f1803ea4ea19af7ce960f41a1b7def8a582
Federico Dittrich <fdittrich@claro.com.ar>
2024-05-14 07:27:52 -0300

    creamos urls, base e index

--- tercerpreentrega/tercerpreentrega/urls.py
+++ tercerpreentrega/tercerpreentrega/urls.py
@@ -19,8 +19,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include("core.urls")),
-    # path('Empresa/', include("Empresa.urls")),
+    path('', include("core.urls")),
+    path('Empresa/', include("Empresa.urls")),
                      
 
 ]
