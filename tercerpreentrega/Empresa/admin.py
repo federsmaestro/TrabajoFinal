0828ac0399bd20e80c1683fd752808697aa880c8
Federico Dittrich <fdittrich@claro.com.ar>
2024-05-14 04:01:50 -0300

    carga de modelos y registro en admin

--- tercerpreentrega/Empresa/admin.py
+++ tercerpreentrega/Empresa/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+admin.site.register(models.Sector)
+admin.site.register(models.Industria)                    
+admin.site.register(models.Pais)
+admin.site.register(models.Empresa)
+
+
+
--- tercerpreentrega/Empresa/models.py
+++ tercerpreentrega/Empresa/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Sector(models.Model):
+    nombre = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
+class Industria(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
+class Empresa(models.Model):
+    nombre = models.PositiveIntegerField(unique=True)
+    sector = models.ForeignKey(Sector, on_delete=models.SET_NULL, null=True, blank=True)
+    pais = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True, blank=True)
+    industria = models.ForeignKey(Industria, on_delete=models.SET_NULL, null=True, blank=True)
