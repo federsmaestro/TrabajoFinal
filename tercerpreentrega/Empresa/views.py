c391ccd0b23b6db688a5b6ce584416bc117ebd79
Federico Dittrich <fdittrich@claro.com.ar>
2024-05-14 08:14:11 -0300

    terminamos views y urls

--- tercerpreentrega/Empresa/views.py
+++ tercerpreentrega/Empresa/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from . import models
+
+def index(request):
+    consulta = models.Empresa.objects.all()
+    contexto = {"empresas":consulta}
+    return render(request,"Empresa/index.html", contexto)
+
