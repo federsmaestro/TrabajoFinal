10f4e798d603198101113de887cb9ecd00afb53b
Federico Dittrich <fdittrich@claro.com.ar>
2024-05-15 16:56:11 -0300

    Con vista basada en clases se armo listado de empresas e ingreso de nueva empresa

--- tercerpreentrega/Empresa/urls.py
+++ tercerpreentrega/Empresa/urls.py
@@ -7,5 +7,7 @@ app_name = "Empresa"
 urlpatterns = [
     
     path('', views.index, name= "index"),                 
+    path('empresa/list', views.EmpresaList.as_view(), name= "empresa_list"),                 
+    path('empresa/create', views.EmpresaCreate.as_view(), name= "empresa_create"),                 
 
 ]
--- tercerpreentrega/Empresa/views.py
+++ tercerpreentrega/Empresa/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
-from . import models
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
+from . import models, forms
 
 def index(request):
-    consulta = models.Empresa.objects.all()
-    contexto = {"empresas":consulta}
-    return render(request,"Empresa/index.html", contexto)
+   
+    return render(request,"Empresa/index.html")
 
+class EmpresaList(ListView):
+    model = models.Empresa
+
+class EmpresaCreate(CreateView):
+    model = models.Empresa
+    form_class=forms.EmpresaForm
+    success_url = reverse_lazy("Empresa:empresa_list")
