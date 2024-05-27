6eb180ae45accc548d314664aeddce9f223e6d18
Federico Dittrich <fdittrich@claro.com.ar>
2024-05-27 02:24:03 -0300

    cambio de nombre proyecto

--- TrabajoFinal/TrabajoFinal/asgi.py
+++ TrabajoFinal/TrabajoFinal/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for tercerpreentrega project.
+ASGI config for TrabajoFinal project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tercerpreentrega.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TrabajoFinal.settings')
 
 application = get_asgi_application()
