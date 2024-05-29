f20e1b3f5ce6492ffdc865e25db019ea9dcd00c2
Federico Dittrich <fdittrich@claro.com.ar>
2024-05-29 05:13:37 -0300

    agrego detail y delete, completo detail

--- TrabajoFinal/Empresa/urls.py
+++ TrabajoFinal/Empresa/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('', views.index, name= "index"),                 
     path('empresa/list', views.EmpresaList.as_view(), name= "empresa_list"),                 
     path('empresa/create', views.EmpresaCreate.as_view(), name= "empresa_create"),                 
-
+    path("empresa/detail/<int:pk>", views.EmpresaDetail.as_view(), name="empresa_detail"),
+    path("empresa/delete/<int:pk>", views.EmpresaDelete.as_view(), name="empresa_delete"),
+    
 ]
